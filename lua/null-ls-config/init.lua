93bc89dc1b9df8706128634b89a855a8dd089c89
Dev Masta <new@mail.com>
2022-03-14 05:39:04 +0700

    why so laggy?

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -5,6 +5,7 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
   sources = {
     formatting.prettierd, formatting.eslint_d,
+    formatting.stylua,
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,55 +1,56 @@
-return require('packer').startup(function(use)
-  -- Core
-  use 'wbthomason/packer.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
+return require("packer").startup(function(use)
+	-- Core
+	use("wbthomason/packer.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("kyazdani42/nvim-tree.lua")
 
-  -- theme-related
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'EdenEast/nightfox.nvim'
-  use 'gruvbox-community/gruvbox'
+	-- theme-related
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use("EdenEast/nightfox.nvim")
+	use("gruvbox-community/gruvbox")
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  -- Legacy
-  use 'wellle/targets.vim'
-  use 'tpope/vim-surround'
-  use 'numToStr/Comment.nvim'
-  use 'mg979/vim-visual-multi'
-  use 'unblevable/quick-scope'
+	-- Legacy
+	use("wellle/targets.vim")
+	use("tpope/vim-surround")
+	use("numToStr/Comment.nvim")
+	use("mg979/vim-visual-multi")
+	use("unblevable/quick-scope")
 
-  -- TreeSitter
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use 'windwp/nvim-ts-autotag'
-  use 'p00f/nvim-ts-rainbow'
-  use 'windwp/nvim-autopairs'
+	-- TreeSitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("windwp/nvim-ts-autotag")
+	use("p00f/nvim-ts-rainbow")
+	use("windwp/nvim-autopairs")
 
-  -- CMP Autocomplete
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+	-- CMP Autocomplete
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/cmp-nvim-lua")
+	use("hrsh7th/nvim-cmp")
 
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/vim-vsnip-integ'
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
+	use("hrsh7th/vim-vsnip-integ")
 
-  use 'onsails/lspkind-nvim'
+	use("onsails/lspkind-nvim")
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
 
-  use 'jose-elias-alvarez/null-ls.nvim'
+	use("jose-elias-alvarez/null-ls.nvim")
 
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'norcalli/nvim-colorizer.lua'
+	use("lukas-reineke/indent-blankline.nvim")
+	use("norcalli/nvim-colorizer.lua")
 end)
