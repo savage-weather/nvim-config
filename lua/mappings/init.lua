0629715a3995bec50c1736624b38bf390e0e8cf5
Dev Masta <new@mail.com>
2022-03-14 06:16:33 +0700

    temp

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,7 +2,7 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = false }
 local term_opts = { silent = true }
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Remaps
 keymap("n", "<C-h>", ":echo 'hello'", opts)
@@ -14,14 +14,14 @@ keymap("i", "<C-s>", "<Esc>:w<cr>", opts)
 keymap("n", "_R", ":Reload<cr>", opts)
 keymap("n", "_r", ":luafile %<cr>", opts)
 keymap("n", "<leader>r", ":luafile %<cr>", opts)
+keymap("n", "<leader>l", ":LspInfo<cr>", opts)
 
-keymap('n', '<leader>q', ':q', opts)
+keymap("n", "<leader>q", ":q", opts)
 
 -- Comment Remaps
-keymap('n', '<C-_>', 'gcc', term_opts)
-keymap('i', '<C-_>', '<Esc>gcc', term_opts)
-keymap('v', '<C-_>', 'gc', term_opts)
-
+keymap("n", "<C-_>", "gcc", term_opts)
+keymap("i", "<C-_>", "<Esc>gcc", term_opts)
+keymap("v", "<C-_>", "gc", term_opts)
 
 -- Insert Mode Remap
 keymap("i", "<A-k>", "<up>", opts)
@@ -30,7 +30,6 @@ keymap("i", "<C-l>", "<right>", opts)
 keymap("i", "<C-h>", "<left>", opts)
 keymap("i", "<C-j>", "<S-left>", opts)
 keymap("i", "<C-k>", "<S-right>", opts)
-
 
 -- Move Cursor Between Windows
 keymap("n", "<C-j>", "<c-w>j", opts)
@@ -42,10 +41,10 @@ keymap("n", "<leader>s", "<c-w>s<c-w>j", opts)
 
 print("__Remapping Done__")
 -- Plug-ins Remaps
-keymap('n', '<leader>e', ':NvimTreeToggle<cr>', opts)
-keymap('n', '<leader>f', ':Telescope find_files<cr>', opts)
-keymap('n', '<c-g>', ':Telescope live_grep<cr>', opts)
-keymap('n', '<c-f>', ':Telescope find_files<cr>', opts)
-keymap('n', '<leader>g', ':Telescope live_grep<cr>', opts)
-keymap('n', '<leader>b', ':Telescope buffers<cr>', opts)
-keymap('n', '<leader>S', ':PackerSync', opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>f", ":Telescope find_files<cr>", opts)
+keymap("n", "<c-g>", ":Telescope live_grep<cr>", opts)
+keymap("n", "<c-f>", ":Telescope find_files<cr>", opts)
+keymap("n", "<leader>g", ":Telescope live_grep<cr>", opts)
+keymap("n", "<leader>b", ":Telescope buffers<cr>", opts)
+keymap("n", "<leader>S", ":PackerSync", opts)
