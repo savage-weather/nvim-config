a7c8fe173a2c79a6680b649eccb3e2bc7a07cd1e
Dev Masta <new@mail.com>
2022-03-14 06:11:32 +0700

    temp

--- init.lua
+++ init.lua
@@ -1,28 +1,26 @@
-require('settings')
-require('packer-config')
-require('nvim-tree-config')
-require('Comment-config')
-require('lualine-config')
-require('treesitter-config')
-require('autopairs-config')
-require('telescope-config')
-require('blankline-config')
-require('lsp')
-require('null-ls-config')
-require('mappings')
+require("settings")
+require("packer-config")
+require("nvim-tree-config")
+require("Comment-config")
+require("lualine-config")
+require("treesitter-config")
+require("autopairs-config")
+require("telescope-config")
+require("blankline-config")
+require("lsp")
+require("null-ls-config")
+require("mappings")
 
 --Color Schemes
 --require('colorschemes-config.nightfox')
-vim.cmd('colorscheme gruvbox')
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
-vim.cmd(':highlight SignColumn guibg=NONE')
+vim.cmd("colorscheme gruvbox")
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+vim.cmd(":highlight SignColumn guibg=NONE")
 
 -- Show Diagnostic in Insert Mode
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    update_in_insert = true,
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	update_in_insert = true,
+})
 
 -- QuickScope Config
 -- Trigger a highlight only when pressing f and F.
@@ -31,4 +29,4 @@ vim.cmd("highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 c
 vim.cmd("highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
 
 -- Colorizer Config
-require'colorizer'.setup()
+require("colorizer").setup()
--- lua/blankline-config/init.lua
+++ lua/blankline-config/init.lua
@@ -1,7 +1,5 @@
-require("indent_blankline").setup {
-  -- for example, context is off by default, use this to turn it on
-  show_current_context = true,
-  show_currejt_context_start = true,
-}
-
-vim.cmd("highlight IndentBlanklineContextChar guifg=#20cfd4 gui=nocombine")
+require("indent_blankline").setup({
+	-- for example, context is off by default, use this to turn it on
+	show_current_context = true,
+	show_currejt_context_start = true,
+})
