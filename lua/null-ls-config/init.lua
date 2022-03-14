12712185a7bff0884afc05e6cf813afa5a69f86d
Dev Masta <new@mail.com>
2022-03-14 08:56:54 +0700

    installed clang_formatter

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -1,15 +1,17 @@
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-  sources = {
-    formatting.prettierd, formatting.eslint_d,
-    formatting.stylua,
-  },
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
-  end
+	sources = {
+		formatting.prettierd,
+		formatting.eslint_d,
+		formatting.stylua,
+		formatting.clang_format,
+	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
 })
